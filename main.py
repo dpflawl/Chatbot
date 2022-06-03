87adb6b654c2d8e493e870b62aa377853da42a3e
dpflawl <89132581+dpflawl@users.noreply.github.com>
2022-06-03 15:43:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -64,13 +64,8 @@ def save_response_content(response, destination):
                 
 @st.cache
 def get_obj_det_model_Drive():
-    # cloud_model_location = "1-EqYjXiygYvJkT6_4peMEN77apMODYA7"
-    # f_checkpoint = Path("KoGPT2Chatbot.pth")    
-    
-    cloud_model_location = "1-DbUxDJROKYSDK-ltSouwnvVGCKOQJto" #kogpt module
-    f_checkpoint = Path("KoGPT2ChatBot.pth")
-    #cloud_model_location = "1o4EDzLHkfqRI_jAtmrzRtOUBjw1iqeQv"
-    #f_checkpoint = Path("KoGPT2Chatbot.pkl")
+    cloud_model_location = "1-EqYjXiygYvJkT6_4peMEN77apMODYA7"
+    f_checkpoint = Path("KoGPT2Chatbot.pth")    
     
     if not f_checkpoint.exists():
         with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):

4781421ec6647c4ea915ff364825043bd0b74f99
dpflawl <89132581+dpflawl@users.noreply.github.com>
2022-06-03 17:51:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -71,12 +71,11 @@ def get_obj_det_model_Drive():
         with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
             download_file_from_google_drive(cloud_model_location, f_checkpoint)
     
-    ##config = GPT2Config()
-    #config = GPT2Config()
+    config = GPT2Config()
     #config.pad_token_id = tokenizer.token_to_id('<pad>')
 
-    ##model = GPT2LMHeadModel(config)
-    model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
+    model = GPT2LMHeadModel(config)
+    ##model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
     model_state_dict = model.state_dict()
     checkpoint = torch.load(f_checkpoint)
     

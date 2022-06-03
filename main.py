004c6bdea27f69d3f37fe3995abae27f95f1866c
dpflawl <89132581+dpflawl@users.noreply.github.com>
2022-06-04 02:04:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,13 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
-    
+U_TKN = '<usr>'
+S_TKN = '<sys>'
+BOS = '</s>'
+EOS = '</s>'
+MASK = '<unused0>'
+SENT = '<unused1>'
+PAD = '<pad>'   
             
 import requests
 
@@ -123,7 +129,7 @@ if user_input:
 
     with torch.no_grad():
         #new_user_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
-        new_user_input_ids = tokenizer.encode(tokenizer.unk_token + user_input + tokenizer.eos_token, return_tensors='pt')
+        new_user_input_ids = tokenizer.encode(tokenizer.unk_token + user_input + '<unused1>' + str(0) + S_TKN + '', return_tensors='pt')
         #tok.encode(U_TKN + q + SENT + sent + S_TKN + a)
         bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids], dim=-1) if 'past' not in st.session_state else new_user_input_ids
         st.session_state.chat_history_ids = model.generate(bot_input_ids,

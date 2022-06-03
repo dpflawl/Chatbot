825a3f79e50cf67450ec9f19128a47fc29a96d82
dpflawl <89132581+dpflawl@users.noreply.github.com>
2022-06-03 14:58:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,8 @@ def get_obj_det_model_Drive():
     #config.pad_token_id = tokenizer.token_to_id('<pad>')
 
     model = GPT2LMHeadModel(config)
-    model.load_state_dict(torch.load(f_checkpoint), strict=False)
+    #model.load_state_dict(torch.load(f_checkpoint), strict=False)
+    model.load_state_dict(f_checkpoint, strict=False)
     #model = GPT2LMHeadModel.load_state_dict(torch.load(f_checkpoint), strict=False)
     model.eval()
     return model

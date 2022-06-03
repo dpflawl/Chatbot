b81e04c0b1e2345aab580489ffedb9a37b1c252c
dpflawl <89132581+dpflawl@users.noreply.github.com>
2022-06-03 20:58:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def get_obj_det_model_Drive():
     model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
    
     model_state_dict = model.state_dict()
-    checkpoint = torch.load(f_checkpoint)
+    checkpoint = f_checkpoint
     
     for key in list(checkpoint.keys()):
       if 'kogpt2.' in key:

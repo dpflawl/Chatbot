d1a5c7c344ee2e6e2cf56efc25c2e7948f0cc270
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-06-06 20:23:57 +0900

    Update main_huggingface.py

--- main_huggingface.py
+++ main_huggingface.py
@@ -135,6 +135,7 @@ if user_input:
         encoded = [tokenizer.encode(user_input)] # 정수 인코딩
         pad_new = tf.keras.preprocessing.sequence.pad_sequences(encoded, maxlen = len(encoded)) # 패딩
         emotion = emo_model(str(user_input))
+        st.write(str(user_input))
         if emotion[0] == "LABEL_0": # 부정
           if emotion[1] < 50: # 부정인 확률 (score)
             emoji = "😶"
